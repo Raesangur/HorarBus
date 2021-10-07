2445d07c356c25800e9c9a0253c5f35d069aa5ac
anthopop123 <72212667+anthopop123@users.noreply.github.com>
2021-10-07 15:08:13 -0400

    vuetify in 2

--- horarbus/src/main.js
+++ horarbus/src/main.js
@@ -1,12 +1,10 @@
 import Vue from "vue";
 import { i18n } from "./plugins/i18n";
 import App from "./App.vue";
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import { router } from "./plugins/router";
 import store from "./store";
 //import Keycloak from "keycloak-js";
-
+import vuetify from '@/plugins/vuetify'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -18,19 +16,16 @@ import "./assets/style/fuckup.css";
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
-Vue.use(Vuetify);
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
-const opts = {}
 
-export default new Vuetify(opts)
-    // let initOptions = {
-    //     url: "http://localhost/auth",
-    //     realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr5vDexhhB+UG5rfzPMKpO8LFgYSnmRvqDoUio/hIo6G9AbZC6UMc9jVB1s10NNFNNbiEl/hvqWE3oMLMvgdZBkeauEP1H/toB2CQkSO+syMLMHkDKYpXoP7Kyfu3/nxgHhBolbdVnORtUSWBxoku4kmm3dOUGOi8dT8O4UiPPCvtee1KzJwdDL/pwKVbPpjP+K3dB6kFjnRnTABJLQu29olnv6zf/9E1NVDmHDwLXIiN7BjAjaYFfkfiJLjDbGD0jGQNWGFFOkYMlprpqsBaf1WzyPY4PEHzc7W6WkR/u5ODCUpBNPoUdK3iSkgT0dMdjOOvvY/YoAROoM5KTdvsHQIDAQAB",
-    //     realm: "usager",
-    //     clientId: "frontend",
-    //     onLoad: "login-required",
-    // };
+// let initOptions = {
+//     url: "http://localhost/auth",
+//     realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr5vDexhhB+UG5rfzPMKpO8LFgYSnmRvqDoUio/hIo6G9AbZC6UMc9jVB1s10NNFNNbiEl/hvqWE3oMLMvgdZBkeauEP1H/toB2CQkSO+syMLMHkDKYpXoP7Kyfu3/nxgHhBolbdVnORtUSWBxoku4kmm3dOUGOi8dT8O4UiPPCvtee1KzJwdDL/pwKVbPpjP+K3dB6kFjnRnTABJLQu29olnv6zf/9E1NVDmHDwLXIiN7BjAjaYFfkfiJLjDbGD0jGQNWGFFOkYMlprpqsBaf1WzyPY4PEHzc7W6WkR/u5ODCUpBNPoUdK3iSkgT0dMdjOOvvY/YoAROoM5KTdvsHQIDAQAB",
+//     realm: "usager",
+//     clientId: "frontend",
+//     onLoad: "login-required",
+// };
 
 //let keycloak = Keycloak(initOptions);
 /////////////////without keycloak
