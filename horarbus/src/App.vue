a4924be817f0354e4025e9ef547d453505b27345
anthopop123 <72212667+anthopop123@users.noreply.github.com>
2021-10-11 17:24:20 -0400

    interface dark mode complete

--- horarbus/src/App.vue
+++ horarbus/src/App.vue
@@ -1,147 +1,16 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" sticky type="dark" class="navbar">
-      <b-navbar-nav
-        class="navbar-menu mx-auto"
-        style="margin-left: 0 !important"
-      >
-        <button class="caret" @click="prev()">
-          <b-icon-caret-left-fill></b-icon-caret-left-fill>
-        </button>
-        <a class="date">
-          {{ today }}
-        </a>
-        <button class="caret" @click="next()">
-          <b-icon-caret-right-fill></b-icon-caret-right-fill>
-        </button>
-      </b-navbar-nav>
-      <b-navbar-nav class="navbar-menu mx-auto">
-        <div class="nom" style="cursor: default">HorarBus</div>
-      </b-navbar-nav>
-      <b-navbar-nav
-        class="navbar-menu mx-auto"
-        style="margin-right: 0 !important"
-      >
-        <b-nav-item-dropdown right class="drop" no-caret>
-          <!-- Using 'button-content' slot -->
-          <template v-slot:button-content>
-            <a class="nom"> John Doe </a>
-          </template>
-          <!--<b-dropdown-item href="/profile"><font-awesome-icon :icon="shoppingCog" /> Profile</b-dropdown-item>-->
-          <b-dropdown-item @click="showPref()">
-            Mes paramètres
-          </b-dropdown-item>
-          <b-dropdown-item
-            >Voir tous les groupes</b-dropdown-item
-          >
-          <b-dropdown-item
-            >Se déconnecter</b-dropdown-item
-          >
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-navbar>
-    <b-modal
-      ref="pref"
-      hide-footer
-      hide-header
-      :centered="true"
-      body-class="preference"
-    >
-      <b-row>
-        <b-col cols="10" class="title"> Mes préférences </b-col>
-        <b-col cols="2" class="zoneClose">
-          <button @click="hidePref()" class="close">x</button>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12" class="sectionPref"> Mode de transport </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12" class="sectionPref"> Notifications </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12" class="sectionPref"> Avance minimum </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12"> </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12" class="sectionPref"> Domicile </b-col>
-      </b-row>
-    </b-modal>
     <router-view />
     <footer>
-      © The Council - Tous droits réservés 2021 / Mise à jour 2021-09-10
+      © The Council - Tous droits réservés 2021 / Mise à jour 2021-10-11
     </footer>
   </div>
 </template>
 
 <script>
-import horaire2Vue from './views/Horaire2.vue';
 export default {
   data: () => ({
-    today: "",
   }),
-  methods: {
-    showPref() {
-      this.$refs["pref"].show();
-    },
-    hidePref() {
-      this.$refs["pref"].hide();
-    },
-    next(){
-      horaire2Vue.next()
-    },
-    prev(){
-      horaire2Vue.prev()
-    },
-  },
-  mounted() {
-    let today = new Date();
-    let month = today.getMonth()+1;
-    switch (month) {
-      case 1:
-        month = "Janvier "
-        break;
-      case 2:
-        month = "Février "
-        break;
-        case 3:
-        month = "Mars "
-        break;
-        case 4:
-        month = "Avril "
-        break;
-        case 5:
-        month = "Mai "
-        break;
-        case 6:
-        month = "Juin "
-        break;
-        case 7:
-        month = "Juillet "
-        break;
-        case 8:
-        month = "Aout "
-        break;
-        case 9:
-        month = "Septembre "
-        break;
-        case 10:
-        month = "Octobre "
-        break;
-        case 11:
-        month = "Novembre "
-        break;
-        case 12:
-        month = "Décembre "
-        break;
-      default:
-        break;
-    }
-    let year = today.getFullYear();
-    this.today = month + year;
-  },
 };
 </script>
 <style>
@@ -150,6 +19,7 @@ footer {
   margin-top: 20px;
   left: 50%;
   transform: translate(-50%, -50%) !important;
+  color: #ffffff;
 }
 .navbar-dark .navbar-nav .nav-link:hover {
   background: transparent;
@@ -217,6 +87,7 @@ footer {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #000000;
 }
 
 #nav {
--- horarbus/src/views/Horaire2.vue
+++ horarbus/src/views/Horaire2.vue
@@ -1,36 +1,155 @@
 <template>
-  <v-calendar
-    ref="calendar"
-    :value="today"
-    :events="events"
-    :event-color="getEventColor"
-    :type="type"
-
-  >
-    <template v-slot:day-body="{ date, week }">
-      <div
-        class="v-current-time"
-        :class="{ first: date === week[0].date }"
-        :style="{ top: nowY }"
-      ></div>
-    </template>
-  </v-calendar>
+  <b-row style="margin-top: 0">
+    <b-col style="padding: 0">
+      <b-navbar toggleable="lg" sticky type="dark" class="navbar">
+        <b-navbar-nav
+          class="navbar-menu mx-auto"
+          style="margin-left: 0 !important"
+        >
+          <button class="caret" @click="prev()">
+            <b-icon-caret-left-fill></b-icon-caret-left-fill>
+          </button>
+          <a class="date" v-if="$refs.calendar" @click="showChoseDate()">
+            {{ this.$refs.calendar.title }}
+          </a>
+          <a class="date" v-else @click="showChoseDate()">
+            {{ today }}
+          </a>
+          <button class="caret" @click="next()">
+            <b-icon-caret-right-fill></b-icon-caret-right-fill>
+          </button>
+        </b-navbar-nav>
+        <b-navbar-nav class="navbar-menu mx-auto">
+          <div class="nom" style="cursor: default">HorarBus</div>
+        </b-navbar-nav>
+        <b-navbar-nav
+          class="navbar-menu mx-auto"
+          style="margin-right: 0 !important"
+        >
+          <b-nav-item-dropdown right class="drop" no-caret>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <a class="nom"> John Doe </a>
+            </template>
+            <!--<b-dropdown-item href="/profile"><font-awesome-icon :icon="shoppingCog" /> Profile</b-dropdown-item>-->
+            <b-dropdown-item @click="showPref()">
+              Mes paramètres
+            </b-dropdown-item>
+            <b-dropdown-item>Voir tous les groupes</b-dropdown-item>
+            <b-dropdown-item>Se déconnecter</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-navbar>
+      <b-modal
+        ref="pref"
+        hide-footer
+        hide-header
+        :centered="true"
+        body-class="preference"
+      >
+        <b-row>
+          <b-col cols="10" class="title"> Mes préférences </b-col>
+          <b-col cols="2" class="zoneClose">
+            <button @click="hidePref()" class="close">x</button>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" class="sectionPref"> Mode de transport </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" class="sectionPref"> Notifications </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" class="sectionPref"> Avance minimum </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12"> </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="12" class="sectionPref"> Domicile </b-col>
+        </b-row>
+      </b-modal>
+      <b-modal
+        ref="chooseDate"
+        hide-footer
+        hide-header
+        body-class="modal-calendar"
+      >
+        <b-row>
+          <b-col cols="10" class="title"> {{ value }} </b-col>
+          <b-col cols="2" class="zoneClose">
+            <button @click="hideChoseDate()" class="close">x</button>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-row>
+              <b-col>
+                <b-calendar
+                  v-model="value"
+                  locale="fr"
+                  :hide-header="true"
+                  label-help=""
+                  block
+                  selected-variant="primary"
+                  today-variant="dark"
+                  nav-button-variant="primary"
+                >
+                </b-calendar>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-button
+                  class="today"
+                  @click="setToday"
+                >
+                  Set Today
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-modal>
+      <v-calendar
+        ref="calendar"
+        :events="events"
+        :dark="true"
+        v-model="value"
+        color="black"
+        locale="fr"
+        event-color="#1867c0"
+        @click:event="showEvent"
+        :type="type"
+      >
+        <template v-slot:day-body="{ date, week }">
+          <div
+            class="v-current-time"
+            :class="{ first: date === week[0].date }"
+            :style="{ top: nowY }"
+          ></div>
+        </template>
+      </v-calendar>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
 export default {
-  next(){
-    this.$refs.calendar.next;
-  },
-  prev(){
-    this.$refs.calendar.prev;
-  },
   data: () => ({
     today: new Date(),
-    value: "",
+    value: new Date(),
     type: "week",
     ready: false,
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+    colors: [
+      "blue",
+      "indigo",
+      "deep-purple",
+      "cyan",
+      "green",
+      "orange",
+      "grey darken-1",
+    ],
     events: [
       {
         name: "Weekly Meeting",
@@ -46,21 +165,22 @@ export default {
         start: "2021-10-14 12:30",
         end: "2021-10-14 15:30",
       },
-       {
+      {
         name: "poutine",
         start: "2021-10-13 12:30",
         end: "2021-10-13 15:30",
       },
     ],
   }),
+
   mounted() {
-   
     this.$refs.calendar.checkChange();
     this.ready = true;
     this.scrollToTime();
-    this.updateTime(); 
-    this.$refs.calendar.next();
+    this.updateTime();
+    this.getToday();
   },
+
   computed: {
     cal() {
       return this.ready ? this.$refs.calendar : null;
@@ -70,6 +190,18 @@ export default {
     },
   },
   methods: {
+    showPref() {
+      this.$refs["pref"].show();
+    },
+    hidePref() {
+      this.$refs["pref"].hide();
+    },
+    showChoseDate() {
+      this.$refs["chooseDate"].show();
+    },
+    hideChoseDate() {
+      this.$refs["chooseDate"].hide();
+    },
     getCurrentTime() {
       return this.cal
         ? this.cal.times.now.hour * 60 + this.cal.times.now.minute
@@ -84,14 +216,70 @@ export default {
     updateTime() {
       setInterval(() => this.cal.updateTimes(), 60 * 1000);
     },
-    getEventColor (event) {
-      return event.color
+    
+    getEventColor(event) {
+      return event.color;
     },
-    next(){
+    next() {
       this.$refs.calendar.next();
     },
-    prev(){
+    prev() {
       this.$refs.calendar.prev();
+    },
+    showEvent() {},
+    setToday() {
+      const now = new Date();
+      let val = now.toISOString().split("T");
+      this.value = val[0];
+    },
+    getToday() {
+      let now = new Date();
+      let month = now.getMonth() + 1;
+      switch (month) {
+        case 1:
+          month = "Janvier ";
+          break;
+        case 2:
+          month = "Février ";
+          break;
+        case 3:
+          month = "Mars ";
+          break;
+        case 4:
+          month = "Avril ";
+          break;
+        case 5:
+          month = "Mai ";
+          break;
+        case 6:
+          month = "Juin ";
+          break;
+        case 7:
+          month = "Juillet ";
+          break;
+        case 8:
+          month = "Aout ";
+          break;
+        case 9:
+          month = "Septembre ";
+          break;
+        case 10:
+          month = "Octobre ";
+          break;
+        case 11:
+          month = "Novembre ";
+          break;
+        case 12:
+          month = "Décembre ";
+          break;
+        default:
+          break;
+      }
+      let year = now.getFullYear();
+      let val = now.toISOString().split("T");
+      this.value = val[0];
+      this.today = month + year;
+      
     },
   },
 };
@@ -113,7 +301,7 @@ export default {
   position: absolute;
   overflow: unset;
 }
-/deep/.v-calendar-daily__scroll-area{
+/deep/.v-calendar-daily__scroll-area {
   overflow: unset;
 }
 .my-event.with-time {
@@ -138,8 +326,204 @@ export default {
   background-color: #ea4335;
   width: 12px;
   height: 12px;
+  left: 0;
   border-radius: 50%;
   margin-top: -5px;
   margin-left: -6.5px;
+}
+
+.navbar-dark .navbar-nav .nav-link:hover {
+  background: transparent;
+}
+
+.nav-item:hover {
+  background-color: transparent;
+}
+.nom {
+  color: #ffffff !important;
+  display: inline-block;
+  font-size: 2.1rem;
+  padding: 0;
+  opacity: 1 !important;
+}
+.drop {
+  display: flex;
+  justify-content: flex-end;
+  width: 186.2px;
+}
+/deep/.dropdown-menu {
+  background-color: #1867c0 !important;
+  color: #ffffff !important;
+}
+.dropdown-item {
+  color: #ffffff !important;
+}
+.dropdown-item:hover,
+.dropdown-item:focus {
+  background-color: #5e5e5e !important;
+}
+.date {
+  font-weight: bold;
+  white-space: nowrap;
+  width: 232px;
+}
+.date:hover {
+  text-decoration: none;
+  color: #ffffff !important;
+}
+.horarbus {
+  display: flex;
+  justify-content: center;
+  width: 186.2px;
+}
+.nom:hover {
+  text-decoration: none;
+  background-color: transparent;
+}
+.navbar-nav {
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+}
+.navbar-dark .navbar-nav .nav-link {
+  color: #ffffff !important;
+  display: inline-block;
+  font-size: 2.1rem;
+  padding: 0;
+  opacity: 1 !important;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+nav ul li {
+  width: 100%;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.navbar {
+  background: #1867c0;
+  flex-wrap: nowrap !important;
+}
+.caret {
+  border: none;
+  background: transparent;
+  color: #ffffff;
+}
+.navbar-menu {
+  height: fit-content;
+}
+nav {
+  height: 56px !important;
+  line-height: 56px !important;
+}
+.zoneClose {
+  vertical-align: middle;
+  margin-top: 5px;
+}
+.close {
+  vertical-align: middle;
+  font-size: 30px !important;
+  color: #ffffff;
+  border: none;
+  background: transparent;
+  text-align: center;
+}
+.preference {
+  background: #da8300;
+  border-radius: 11px;
+  color: #ffffff;
+}
+/deep/.modal-calendar {
+  background: #2b2b2b;
+  border-radius: 11px;
+  color: #ffffff;
+}
+.title {
+  font-size: 25px;
+  color: #ffffff;
+  font-weight: bold;
+}
+.modal .modal-content {
+  padding: 0 !important;
+  border-radius: 15px;
+}
+.sectionPref {
+  font-size: 13px;
+  font-weight: bold;
+}
+/deep/.theme--dark.v-calendar-events .v-event-timed {
+  color: #ffffff !important;
+  font-size: 15px;
+  font-weight: bolder;
+  border: 0 !important;
+}
+/deep/.v-calendar .v-event {
+  color: #ffffff !important;
+  font-size: 14px;
+  font-weight: bolder;
+  border: 0 !important;
+}
+.today {
+  border-radius: 5px;
+  width: 100%;
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: bolder;
+  background: transparent;
+}
+.today:hover {
+  border-radius: 5px;
+  width: 100%;
+  color: #000000;
+  background: #ffffff;
+}
+/deep/.btn-outline-primary{
+  color: #ffffff !important;
+  border-color: none !important;
+  background: #007bff;
+}
+/deep/.btn-outline-primary:hover{
+  color: #ffffff !important;
+  border-color: none !important;
+  background: #007bff;
+  opacity: 0.9;
+}
+/deep/.btn-outline-dark {
+  color: #ffffff !important;
+  border-color: none !important;
+  background: #343a40;
+}
+/deep/.btn-outline-dark:hover{
+  color: #ffffff !important;
+  border-color: none !important;
+  background: #0062cc;
+}
+/deep/.btn-outline-light {
+  color: #f8f9fa;
+  background-color: #f8f9fa;
+}
+/deep/.btn-outline-light:hover {
+  color: #ffffff !important;
+  border-color: none !important;
+  background: #0062cc;
+}
+/deep/.btn-light{
+  color: #ffffff !important;
+  border-color: none !important;
+  background: #007bff !important;
 }
 </style>
