1cca826ead0c04f6fd6268a6568657aef8ea6d29
Julien Rousseau <julrousseau20@gmail.com>
2021-10-20 13:29:30 -0400

    Almost working setup

--- server/src/main/java/com/horarbus/ExampleResource.java
+++ server/src/main/java/com/horarbus/ExampleResource.java
@@ -4,8 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 import java.util.Scanner;
 
 @Path("/hello")
@@ -14,30 +15,31 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        try{
+        try {
             String apiKey = getGoogleApiKey();
             return "Hello RESTEasy" + apiKey;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return "Could not find the google key";
         }
     }
 
-    private String getGoogleApiKey() throws Exception{
-        try{
-            File envFile = new File("../../../../.env");
-            Scanner fileReader = new Scanner(envFile);
-            while(fileReader.hasNextLine()){
-                String envVar = fileReader.nextLine();
-                String[] envVarParts = envVar.split("=");
+    private String getGoogleApiKey() throws Exception {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("config.properties");
+        if (stream != null) {
+            try {
+                InputStreamReader streamReader = new InputStreamReader(stream, StandardCharsets.UTF_8);
+                BufferedReader reader = new BufferedReader(streamReader);
 
-                if(envVarParts[0].equals("GOOGLE_API_KEY")){
-                    return envVarParts[1];
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (line.split("=")[0].equals("googleApiKey")) {
+                        return line.split("=")[1];
+                    }
                 }
+            } catch (IOException ex) {
+                throw new Exception(ex);
             }
-            fileReader.close();
-            throw new Exception("Could not find the API key");
-        }catch (FileNotFoundException ex){
-            throw new Exception(ex);
         }
+        throw new Exception("Could not load config.properties.");
     }
 }
