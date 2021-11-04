675f2b1c496925766c102b5088fd82d1431abed0
aliseeperreault <pera3307@usherbrooke.ca>
2021-11-04 15:02:04 -0400

    paramètre : temps d'avance & domicil

--- web/src/views/Horaire.vue
+++ web/src/views/Horaire.vue
@@ -140,19 +140,30 @@
         </b-row>
 
         <b-row>
-            <input v-model.number="temps" type="number" class="tempsSelect">
-            <p>Message is: {{ temps }}</p>
+            <input v-model.number="temps_avance_notification" type="number" class="tempsSelect">
         </b-row>
 
+        <!--Temps d'avance minimum-->
         <b-row>
           <b-col cols="12" class="sectionPref"> Avance minimum </b-col>
+        </b-row>
+          <b-row>
+            <input v-model.number="temps_avance" type="number" class="tempsSelect">
         </b-row>
         <b-row>
           <b-col cols="12"> </b-col>
         </b-row>
+
+          <!--Domicil-->
         <b-row>
           <b-col cols="12" class="sectionPref"> Domicile </b-col>
         </b-row>
+
+        <b-row>
+            <input v-model="adresse_maison" placeholder="Adresse du domicil" class="tempsSelect">
+        </b-row>
+
+          
         <b-row>
           <b-col cols="12" class="sectionPref">
             Dark Mode
@@ -271,6 +282,8 @@ export default {
   },
 
   data: () => ({
+    adresse_maison:"",
+    temps_avance: 10,
     transport: "bus",
     notification_enable: true,
     transportOption: [
