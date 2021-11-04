6f9c3dd51ca06a4cdd44f3137f60bf4ed22846d8
Julien Rousseau <julrousseau20@gmail.com>
2021-11-03 22:57:36 -0400

    Initial keycloak token validation

--- server/src/main/java/com/horarbus/auth/AuthData.java
+++ server/src/main/java/com/horarbus/auth/AuthData.java
@@ -4,11 +4,13 @@ public class AuthData {
     private String cip;
     private String firstname;
     private String lastname;
+    private String email;
 
-    public AuthData(String cip, String firstname, String lastname) {
+    public AuthData(String cip, String firstname, String lastname, String email) {
         this.cip = cip;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.email = email;
     }
 
     public String getCip() {
@@ -21,6 +23,10 @@ public class AuthData {
 
     public String getLastname() {
         return lastname;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
--- server/src/main/java/com/horarbus/auth/AuthFilter.java
+++ server/src/main/java/com/horarbus/auth/AuthFilter.java
@@ -1,14 +1,16 @@
 package com.horarbus.auth;
 
+import io.vertx.core.json.JsonObject;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 @Provider
 @PreMatching
@@ -16,12 +18,9 @@ public class AuthFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        // TODO: filter keycloak
-
         try {
-            System.out.println("TOKEN: " + getAuthToken(requestContext));
-
-            AuthData authData = new AuthData("rouj1615", "Julien", "S!X");
+            String token = getAuthToken(requestContext);
+            AuthData authData = validateToken(token);
             requestContext.setProperty("authData", authData);
         } catch (AuthException ex) {
             rejectRequest(requestContext);
@@ -34,6 +33,45 @@ public class AuthFilter implements ContainerRequestFilter {
         } catch (Exception e) {
             throw new AuthException("Missing auth token");
         }
+    }
+
+    private AuthData validateToken(String token) throws AuthException {
+        String validationEndpoint = "http://localhost/auth/realms/usager/protocol/openid-connect/userinfo";
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(validationEndpoint);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Length", Integer.toString(token.getBytes(StandardCharsets.UTF_8).length));
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Authorization", token);
+
+            InputStream inStream = connection.getInputStream();
+            BufferedReader readStream = new BufferedReader(new InputStreamReader(inStream));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = readStream.readLine()) != null) {
+                response.append(line);
+                response.append("\r");
+            }
+            readStream.close();
+
+            JsonObject json = new JsonObject(response.toString());
+            return new AuthData(json.getString("preferred_username"), json.getString("given_name"), json.getString("family_name"), json.getString("email"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        throw new AuthException("idk");
+
     }
 
     private void rejectRequest(ContainerRequestContext context) {
