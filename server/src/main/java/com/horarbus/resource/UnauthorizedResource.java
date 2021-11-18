570e0ea319cbc4dcd34a202c6d50b6de298c7375
Julien Rousseau <julrousseau20@gmail.com>
2021-11-18 02:18:33 -0500

    Added itinerary resource

--- server/src/main/java/com/horarbus/resource/UnauthorizedResource.java
+++ server/src/main/java/com/horarbus/resource/UnauthorizedResource.java
@@ -5,22 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import com.horarbus.service.MapsService;
 
 @Path("/unauthorized")
 public class UnauthorizedResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response getUnauthorized() {
-        String res = "";
-
-        try {
-            res = MapsService.getPlaceDataFromId("ChIJ3zeuCDmzt0wR-OmeoeEEspM");
-        } catch (Exception ex) {
-            res = "An error occured.";
-        }
-
-        return Response.status(Response.Status.UNAUTHORIZED).entity(res).type(MediaType.TEXT_PLAIN)
-                .build();
+        return Response.status(Response.Status.UNAUTHORIZED).type(MediaType.TEXT_PLAIN).build();
     }
 }
